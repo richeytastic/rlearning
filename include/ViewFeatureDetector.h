698d2e0706d696717ec874ca02337df638ba7e8e
Richard Palmer <richeytastic@gmail.com>
2017-08-04 20:04:31 +0800

    Added GPL v3 license headers to .h and .cpp files.

--- include/ViewFeatureDetector.h
+++ include/ViewFeatureDetector.h
@@ -1,3 +1,20 @@
+/************************************************************************
+ * Copyright (C) 2017 Richard Palmer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ************************************************************************/
+
 /**
  * Common feature detection interface for 3-D images (encapsulated as View objects).
  * Defines image scanning method and defers detection itself to a FeatureDetector object.
--- include/template/SVMParams_template.h
+++ include/template/SVMParams_template.h
@@ -1,3 +1,20 @@
+/************************************************************************
+ * Copyright (C) 2017 Richard Palmer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ************************************************************************/
+
 template <typename T>
 SVMParams::SVMParams( double cost, double eps, const boost::shared_ptr<KernelFunc<T> > k)
     : cost_(cost), eps_(eps), kernel_(LinearKernel<T>::Type), gamma_(1), coef0_(0), degree_(1)
